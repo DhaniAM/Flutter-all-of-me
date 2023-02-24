05acc755e0720ccfa9d08aec2d3164336694e56b
Dhani AM <dhani.a.mm@gmail.com>
2023-02-24 17:56:02 +0800

    upgraded flutter to version 3.7.5

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -14,7 +14,7 @@ class AboutPage extends StatelessWidget {
 
     double screenWidth = MediaQuery.of(context).size.width;
     return Container(
-      color: Colors.yellow,
+      color: Colors.cyan[100],
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
