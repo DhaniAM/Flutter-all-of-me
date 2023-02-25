f9525c8b0a053602599e1924742eef4e3c272c12
Dhani AM <dhani.a.mm@gmail.com>
2023-02-25 17:27:03 +0800

    added education and organization experience in CV

--- lib/cv_page.dart
+++ lib/cv_page.dart
@@ -12,6 +12,7 @@ class _CvPageState extends State<CvPage> {
   static const TextStyle _headerStyle = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.italic,
   );
 
   final List<bool> _isOpen = [
@@ -33,11 +34,14 @@ class _CvPageState extends State<CvPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const Text(
-                'Curriculum Vitae',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w300,
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                child: Text(
+                  'Curriculum Vitae',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w300,
+                  ),
                 ),
               ),
               ExpansionPanelList(
@@ -59,7 +63,36 @@ class _CvPageState extends State<CvPage> {
                         ),
                       );
                     },
-                    body: Text('content'),
+                    body: Padding(
+                      padding: const EdgeInsets.only(left: 30.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          // Polanka
+                          const Text('Assosiate Degree'),
+                          const Text('Medical Laboratory Technologist'),
+                          const Text('2017 - 2020'),
+                          Row(
+                            children: const [
+                              Icon(Icons.school),
+                              Text(' Politeknik Unggulan Kalimantan'),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+
+                          // Unsia
+                          const Text('Bachelor Degree'),
+                          const Text('Computer Science'),
+                          const Text('2022 - now'),
+                          Row(
+                            children: const [
+                              Icon(Icons.school),
+                              Text(' Universitas Siber Asia'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                     isExpanded: _isOpen[0],
                     canTapOnHeader: true,
                   ),
@@ -73,7 +106,49 @@ class _CvPageState extends State<CvPage> {
                         ),
                       );
                     },
-                    body: Text('content'),
+                    body: Padding(
+                      padding: const EdgeInsets.only(left: 30, right: 30),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: const [
+                          // BEM 1
+                          Text('Student Executive Council'),
+                          Text(
+                              'Staff of Communication & Information Department'),
+                          Text('2017 - 2018'),
+                          SizedBox(height: 20),
+
+                          // HIMA 1
+                          Text(
+                              'Student Association of Medical Laboratory Technologist'),
+                          Text('Staff of External & Internal Division'),
+                          Text('2017 - 2018'),
+                          SizedBox(height: 20),
+
+                          // BEM 2
+                          Text('Student Executive Council'),
+                          Text(
+                              'Head of Communication & Information Department'),
+                          Text('2018 - 2019'),
+                          SizedBox(height: 20),
+
+                          // HIMA 2
+                          Text(
+                              'Student Association of Medical Laboratory Technologist'),
+                          Text(
+                              'Staff of Communication & Information Department'),
+                          Text('2018 - 2019'),
+                          SizedBox(height: 20),
+
+                          // BEM 2
+                          Text('Medical Laboratory Technologist Seminar'),
+                          Text(
+                              'Coordinator of Communication & Information Division'),
+                          Text('2019 - 2020'),
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                    ),
                     isExpanded: _isOpen[1],
                     canTapOnHeader: true,
                   ),
