2d3c888b2869d0739c65545b185965dcde5a4efd
Dhani AM <dhani.a.mm@gmail.com>
2023-03-06 13:01:08 +0800

    added MyImage and MyLogo class

--- lib/my_constant.dart
+++ lib/my_constant.dart
@@ -9,26 +9,43 @@ class MyColor {
   static const Color myLightGreen = Color(0xffABDFDD);
   static const Color myLightGreen2 = Color(0xffC8EAE8);
 }
-//
-// class MyImage {
-//   static const String ai = 'assets/logo/ae.png';
-//   static const String ae = 'assets/logo/ae.png';
-//   static const String android = 'assets/logo/ae.png';
-//   static const String aws = 'assets/logo/ae.png';
-//   static const String cpd = 'assets/logo/ae.png';
-//   static const String cpp = 'assets/logo/ae.png';
-//   static const String css = 'assets/logo/ae.png';
-//   static const String dart = 'assets/logo/ae.png';
-//   static const String devops = 'assets/logo/ae.png';
-//   static const String figma = 'assets/logo/ae.png';
-//   static const String git = 'assets/logo/ae.png';
-//   static const String github = 'assets/logo/ae.png';
-//   static const String html = 'assets/logo/ae.png';
-//   static const String java = 'assets/logo/ae.png';
-//   static const String js = 'assets/logo/ae.png';
-//   static const String node = 'assets/logo/ae.png';
-//   static const String  = 'assets/logo/ae.png';
-//   static const String java = 'assets/logo/ae.png';
-//   static const String java = 'assets/logo/ae.png';
-//
-// }
+
+class MyImage {
+  static const String binus = 'assets/img/binus.png';
+  static const String cakap = 'assets/logo/cakap.png';
+  static const String dicoding = 'assets/logo/dicoding.png';
+  static const String github = 'assets/logo/github.png';
+  static const String gmail = 'assets/logo/gmail.png';
+  static const String indo = 'assets/logo/indo.png';
+  static const String linkedin = 'assets/logo/linkedin.png';
+  static const String profile = 'assets/logo/profile.png';
+  static const String progate = 'assets/logo/progate.png';
+  static const String sololearn = 'assets/logo/sololearn.png';
+  static const String uk = 'assets/logo/uk.png';
+  static const String web = 'assets/logo/web.png';
+  static const String whatsapp = 'assets/logo/whatsapp.png';
+}
+
+class MyLogo {
+  static const String ai = 'assets/logo/ae.png';
+  static const String ae = 'assets/logo/ae.png';
+  static const String android = 'assets/logo/android.png';
+  static const String aws = 'assets/logo/aws.png';
+  static const String cpd = 'assets/logo/cpd.png';
+  static const String cpp = 'assets/logo/cpp.png';
+  static const String css = 'assets/logo/css.png';
+  static const String dart = 'assets/logo/dart.png';
+  static const String devops = 'assets/logo/devops.png';
+  static const String figma = 'assets/logo/figma.png';
+  static const String git = 'assets/logo/git.png';
+  static const String github = 'assets/logo/github.png';
+  static const String html = 'assets/logo/html.png';
+  static const String java = 'assets/logo/java.png';
+  static const String js = 'assets/logo/js.png';
+  static const String node = 'assets/logo/node.png';
+  static const String pr = 'assets/logo/pr.png';
+  static const String ps = 'assets/logo/ps.png';
+  static const String python = 'assets/logo/python.png';
+  static const String react = 'assets/logo/react.png';
+  static const String sql = 'assets/logo/sql.png';
+}
